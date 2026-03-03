26f3430bbcbfe96f49464e9c4ac068c0a51ffe47
Ranjeet6639 <ranjeet.shinde6639@gmail.com>
2026-03-03 16:26:41 +0530

    resolve mobile navbar hiding issue and add smooth slide animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Home.css";
-
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // 👇 Automatically close menu when route changes
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location]);
 
   return (
     <div className="navbar animate">
@@ -18,17 +23,15 @@ function Navbar() {
       </button>
 
       <div className={`nav-links animate ${menuOpen ? "show" : ""}`}>
-      <Link to="/">Home</Link>
-      <Link to="/farmer-register">Farmer Register</Link>
-      <Link to="/trader-register">Trader Register</Link>
-      <Link to="/farmer-login">Farmer Login</Link>
-      <Link to="/trader-login">Trader Login</Link>
-      <Link to="/contact">Contact Us</Link>
-      
-     </div>
+        <Link to="/">Home</Link>
+        <Link to="/farmer-register">Farmer Register</Link>
+        <Link to="/trader-register">Trader Register</Link>
+        <Link to="/farmer-login">Farmer Login</Link>
+        <Link to="/trader-login">Trader Login</Link>
+        <Link to="/contact">Contact Us</Link>
+      </div>
     </div>
   );
 }
-
 
 export default Navbar;
